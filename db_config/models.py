6d9212b4b3bd25565172bfc68f3d77a43ba7806d
Владимир <baranov-v-k@yandex.ru>
2025-02-19 07:20:05 +0300

    Добавление конфигурации для базы данных в проект

---
+++ db_config/models.py
@@ -0,0 +1,14 @@
+from db_config.db_connection import get_connection
+
+with get_connection() as conn:
+    with conn.cursor() as curs:
+        curs.execute('''CREATE TABLE IF NOT EXISTS orders
+        (ID INT PRIMARY KEY NOT NULL,
+        CREATION_TIME TIMESTAMP NOT NULL,\
+        CHANGE_TIME TIMESTAMP NOT NULL,
+        STATUS TEXT NOT NULL,
+        SIDE TEXT NOT NULL,
+        PRICE real NOT NULL,
+        AMOUNT real NOT NULL,
+        INSTRUMENT TEXT NOT NULL);''')
+        conn.commit()
